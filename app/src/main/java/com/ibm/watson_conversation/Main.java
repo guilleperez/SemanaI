7bae4bbf7c3d27afcceaf92ea0012494d720efd9
guilleperez <guillermopt@outlook.com>
2017-09-28 10:38:52 -0500

    ESTE SIRVE

--- app/src/main/java/com/ibm/watson_conversation/Main.java
+++ app/src/main/java/com/ibm/watson_conversation/Main.java
@@ -14,12 +14,20 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     
-        watson = (Button) findViewById(R.id.button);
+        //watson = (Button) findViewById(R.id.button);
         registrar = (Button) findViewById(R.id.button2);
-        watson.setOnClickListener(new View.OnClickListener() {
+       /* watson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent inti = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(inti);
+            }
+        });*/
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inti = new Intent(getBaseContext(), Registrar.class);
                 startActivity(inti);
             }
         });
