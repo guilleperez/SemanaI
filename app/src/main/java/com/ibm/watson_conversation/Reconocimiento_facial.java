8c173075461bc759b39b0f92d61e5fe8efdf2103
Yuso Malo <yusoazul77@hotmail.com>
2017-09-28 21:33:32 -0500

    ddddd

--- app/src/main/java/com/ibm/watson_conversation/Reconocimiento_facial.java
+++ app/src/main/java/com/ibm/watson_conversation/Reconocimiento_facial.java
@@ -1,22 +1,15 @@
 package com.ibm.watson_conversation;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.io.File;
-
 public class Reconocimiento_facial extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
